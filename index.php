361d4b043db132d09d06ff0398e2ed5a8977daa9
Sub Estacion <subestacion@paseolagaleria.com>
2019-12-08 21:03:15 -0300

    Se agregan archivos y estilos bootswatch para formato app

--- index.php
+++ index.php
@@ -5,35 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/estilos.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/custom.min.css">
     <link rel="shortcut icon" href="iconos/electrico.ico" type="image/x-icon">
     <title>Login</title>
 </head>
-<body>
-    <div class="container-fluid">
+<body class="bg-primary">
+    <div class="container">
         <div class="row">
-    
-            <div class="col-lg-12 col-md-12 cont">
-                <h1 class="text-center">Planilla de Tareas - Eléctrica</h1>
+        <header class="col-lg-12 col-md-12 col-sm-8 fixed-top mt-5">
+            <h3 class="text-center font-weight-normal">Tareas Eléctrica</h3>
+        </header>
+        </div>
+        <div class="row border border-dark mx-3 h-100">
+            <div class="col-lg-12 col-md-12 col-sm-8 cont py-5">
                     <form method="post" action="./validaciones/validar_usuario.php">
                         <div class="form-group">
-                            <label>Usuario</label>
+                            <label><b>Usuario</b></label>
                             <input type="text" class="form-control w-50" name="usuario" placeholder="Usuario">
                         </div>
                         <div class="form-group">
-                            <label>Password</label>
+                            <label><b>Password</b></label>
                             <input type="password" class="form-control w-50" name="pass" placeholder="Password">
                         </div>
-                            <button type="submit" class="btn btn-primary">Ingresar</button>
+                            <button type="submit" class="btn btn-outline-info"><b>Ingresar</b></button>
                     </form>
                 
             </div>
         </div>
     </div>
     
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/custom.js"></script>
+  
 </body>
 </html>
