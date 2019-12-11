d502566c452e26c2c7159085b7b46e59f51bcf97
Sub Estacion <subestacion@paseolagaleria.com>
2019-12-11 13:15:42 -0300

    Se cambia frontend del login

--- index.php
+++ index.php
@@ -1,44 +1,100 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/estilos.css">
-    <link rel="stylesheet" href="./css/bootstrap.css">
-    <link rel="stylesheet" href="./css/custom.min.css">
-    <link rel="shortcut icon" href="iconos/electrico.ico" type="image/x-icon">
-    <title>Login</title>
+	<title>Tareas Electrica</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="shortcut icon" href="iconos/electrico.ico" type="image/x-icon">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================>
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css"-->
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
-<body class="bg-primary">
-    <div class="container">
-        <div class="row">
-        <header class="col-lg-12 col-md-12 col-sm-8 fixed-top mt-5">
-            <h3 class="text-center font-weight-normal">Tareas Eléctrica</h3>
-        </header>
-        </div>
-        <div class="row border border-dark mx-3 h-100">
-            <div class="col-lg-12 col-md-12 col-sm-8 cont py-5">
-                    <form method="post" action="./validaciones/validar_usuario.php">
-                        <div class="form-group">
-                            <label><b>Usuario</b></label>
-                            <input type="text" class="form-control w-50" name="usuario" placeholder="Usuario">
-                        </div>
-                        <div class="form-group">
-                            <label><b>Password</b></label>
-                            <input type="password" class="form-control w-50" name="pass" placeholder="Password">
-                        </div>
-                            <button type="submit" class="btn btn-outline-info"><b>Ingresar</b></button>
-                    </form>
-                
-            </div>
-        </div>
-    </div>
-    
-    <script src="./js/jquery.min.js"></script>
-    <script src="./js/popper.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/custom.js"></script>
-  
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100 bg-info">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" method="post" action="./validaciones/validar_usuario.php">
+					<span class="login100-form-title p-b-26">
+						Planilla Eléctrica
+					</span>
+					<span class="login100-form-title p-b-48">
+						<i class="zmdi zmdi-font"></i>
+					</span>
+
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" name="usuario">
+						<span class="focus-input100" data-placeholder="Usuario"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">
+								Acceder
+							</button>
+						</div>
+					</div>
+
+					<div class="text-center p-t-115">
+						<a class="txt2 d-block" href="#">
+							Sign Up
+						</a>
+						<span class="txt1">
+							¿Has olvidado tu cuenta?
+						</span>
+
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
